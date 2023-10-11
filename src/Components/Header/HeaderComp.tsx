618f9dc540ddc94d9a3056640d66e96773447bf5
shaneTF <shanefranklin148@gmail.com>
2023-10-10 21:55:22 -0400

    Changed the text size to be more appropiate for a header

--- src/Components/Header/HeaderComp.tsx
+++ src/Components/Header/HeaderComp.tsx
@@ -3,10 +3,10 @@ import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
-      <div className="grid grid-col-1 w-full place-items-center">
-        <h5>Hello! I am</h5>
-        <h1>Shane Franklin</h1>
-        <h5>Software Engineer</h5>
+      <div className="grid grid-col-1 w-full place-items-center p-5">
+        <h5 className="text-xl">Hello! I am</h5>
+        <h1 className="text-4xl">Shane Franklin</h1>
+        <h5 className="text-xl">Software Engineer</h5>
         <HeaderSocials />
       </div>
     </header>
