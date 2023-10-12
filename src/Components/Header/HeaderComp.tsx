3acdae9954d91132ca78e35767407a603b828929
shaneTF <shanefranklin148@gmail.com>
2023-10-12 18:45:23 -0400

    Finished up header styling for now

--- src/Components/Header/HeaderComp.tsx
+++ src/Components/Header/HeaderComp.tsx
@@ -2,11 +2,13 @@ import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
-    <header>
-      <div className="grid grid-col-1 w-full place-items-center p-5">
+    <header className="m-auto relative p-10 w-3/4">
+      <div className="grid grid-col-1 place-items-center p-5">
         <h5 className="text-xl">Hello! I am</h5>
         <h1 className="text-4xl">Shane Franklin</h1>
         <h5 className="text-xl">Software Engineer</h5>
+      </div>
+      <div>
         <HeaderSocials />
       </div>
     </header>
